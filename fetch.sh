d4e908fbaaffd7c1f5aac7071c95ae0e7e3b0337
Nathaniel Gaertner <natgaertner@gmail.com>
2015-07-26 18:12:46 -0700

    add correct path to python path

--- fetch.sh
+++ fetch.sh
@@ -2,4 +2,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 chmod 755 $DIR/op_config_tool.py
 aws s3 cp s3://${OP_CONFIG_BUCKET}/${OP_CONFIG_FILE:-op.cfg} $DIR/op.cfg
 alias opc="$DIR/op_config_tool.py $DIR/op.cfg"
-export PYTHONPATH=$(pwd):$PYTHONPATH
+export PYTHONPATH=$DIR:$PYTHONPATH
