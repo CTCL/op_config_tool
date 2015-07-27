2234ce7fab4a056314d009595cd049184d449c20
Nathaniel Gaertner <natgaertner@gmail.com>
2015-07-27 14:09:38 -0700

    fix fetch file finding

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 aws s3 cp s3://${OP_CONFIG_BUCKET}/${OP_CONFIG_FILE:-op.cfg} $OP_CONFIG_PATH/op.cfg
-if [ ! -a $OP_CONFIG_PATH/op.cfg ]
+if ! [ -a $OP_CONFIG_PATH/op.cfg ]
 then
+  echo 'could not get op config file'
   echo '[op]' > $OP_CONFIG_PATH/op.cfg
 fi
