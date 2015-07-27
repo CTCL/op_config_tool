a2e6c727cfb78041035e2cefc4b1e1ae47b5d52e
Nathaniel Gaertner <natgaertner@gmail.com>
2015-07-27 13:22:47 -0700

    find location correctly

--- fetch_op_config.py
+++ fetch_op_config.py
@@ -4,7 +4,7 @@ from subprocess import call
 def run(remote_file=None):
     if not remote_file:
         remote_file = os.getenv("OP_CONFIG_FILE", "op.cfg")
-    location = os.getenv('OP_CONFIG_PATH')
+    location = os.path.dirname(os.path.abspath(__file__))
     op_config_file_path = os.path.join(location, "op.cfg")
     call(["aws", "s3", "cp", "s3://{bucket}/{remote_file}".format(bucket=os.getenv("OP_CONFIG_BUCKET"), remote_file=remote_file), op_config_file_path])
     if not os.path.isfile(op_config_file_path):
