dd43a3d421aeabd9feac3f6bd8993ac97909c312
Nathaniel Gaertner <natgaertner@gmail.com>
2015-07-27 13:43:10 -0700

    clean up fetch.sh

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-$(aws s3 cp s3://${OP_CONFIG_BUCKET}/${OP_CONFIG_FILE:-op.cfg} $OP_CONFIG_PATH/op.cfg)
+aws s3 cp s3://${OP_CONFIG_BUCKET}/${OP_CONFIG_FILE:-op.cfg} $OP_CONFIG_PATH/op.cfg
 if [ ! -a $OP_CONFIG_PATH/op.cfg ]
 then
   echo '[op]' > $OP_CONFIG_PATH/op.cfg
